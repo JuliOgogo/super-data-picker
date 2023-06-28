da987f59e217698127d531ba69f9deb2dea470e9
Julia Titkova <juli.ogogo@yandex.ru>
2023-06-28 21:52:23 +0300

    feat: local storage functions and settings

--- src/components/SuperDataPicker/SuperDataPicker.tsx
+++ src/components/SuperDataPicker/SuperDataPicker.tsx
@@ -1,20 +1,36 @@
 import React, {useState} from 'react';
 import ReactDatePicker from 'react-datepicker';
+import {setToLocalStorage} from '../../common/localStorage';
 
 export function SuperDataPicker() {
 
     const [startDate, setStartDate] = useState<Date>()
     const [endDate, setEndDate] = useState<Date>()
+    const [isSettingsOpen, setIsSettingsOpen] = useState(false)
 
     const onStartChangeHandler = (date: Date | null) => {
-        if (date) setStartDate(date)
+        if (date) {
+            setStartDate(date)
+            setToLocalStorage('startDate', date)
+        }
     }
 
     const onEndChangeHandler = (date: Date | null) => {
-        if (date) setEndDate(date)
+        if (date) {
+            setEndDate(date)
+            setToLocalStorage('endDate', date)
+        }
+    }
+
+    const onClickHandler = () => {
+        setStartDate(undefined)
+        setEndDate(undefined)
     }
 
     return <div>
+
+        <Settings isSettingsOpen={isSettingsOpen}/>
+
         <ReactDatePicker
             selectsStart
             dateFormat={'dd-MM-yyyy'}
@@ -37,5 +53,17 @@ export function SuperDataPicker() {
             selected={endDate}
             onChange={(date) => onEndChangeHandler(date)}
             minDate={startDate}/>
+
+        <button onClick={onClickHandler}>Refresh</button>
+    </div>
+}
+
+type SettingsPropsType = {
+    isSettingsOpen: boolean
+}
+
+export const Settings: React.FC<SettingsPropsType> = ({isSettingsOpen}) => {
+    return <div>
+        Settings
     </div>
 }
